2ee379ad2867c9c4e7b22a84ad7f93e3d43539a3
Viji Vidyadharan <vvidyadharan@hotmail.com>
2018-10-04 17:45:54 -0500

    Add PST /todos route and refactor mongoose

---
+++ server/db/mongoose.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp', {
+    useNewUrlParser: true
+});
+
+module.exports = {mongoose}
+/* before ES6
+module.exports = {
+    mongoose: mongoose
+}
+*/
