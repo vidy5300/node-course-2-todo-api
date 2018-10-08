4ae1bd9f118f11902b48429f1dc0738add320277
Viji Vidyadharan <vvidyadharan@hotmail.com>
2018-10-08 17:50:30 -0500

    Add Get /todos route

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    console.log(req.body);
     var todo = new Todo({
         text: req.body.text
     });
@@ -24,6 +23,14 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+})
 
 
 module.exports = {app};
